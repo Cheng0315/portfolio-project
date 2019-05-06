8f33571bff9f2349b923d27907073adc480759ba
Cheng Vue <csalvue@gmail.com>
2019-05-06 00:04:06 -0700

    add 'about me' paragraph to aboutMe component

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -5,8 +5,15 @@ class AboutMe extends React.Component {
   render() {
     return (
       <div className='about-me-container'>
+        <img src='https://ak5.picdn.net/shutterstock/videos/26581145/thumb/1.jpg'/>
         <div className='about-me'>
-          
+          <h1>Full Stack Web Developer</h1>
+          <p>Full Stack Web Developer with a passion for building innovative and 
+            user friendly web application. With experience in Ruby on Rails, JavaScript, 
+            and React and a background in music education, I discovered web development 
+            through my interest in making music using software such as Pro Tools and Ableton Live. 
+            An eager learner, I bring strong skills in team-building and project management 
+            that help further advance company objectives.</p>
         </div>
       </div>
     )
